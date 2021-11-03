e9fd25e4fb3e871af39351eca744b0d181fdfacc
Kalley Matheus <matheusmonck@gmail.com>
2021-11-02 22:40:48 -0300

    aula-2 end

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
-import firebase from "firebase/compat/app";
+import firebase from "firebase/app";
 
-import "firebase/compat/auth";
-import "firebase/compat/database";
+import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,5 +15,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
